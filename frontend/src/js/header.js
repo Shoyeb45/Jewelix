d8f49e91f3a3586f83199a4a877e201159f8dcc1
Shoyeb45 <shoyebff45@gmail.com>
2024-12-27 17:59:29 +0530

    Header and nav made by ankit

---
+++ frontend/src/js/header.js
@@ -0,0 +1,10 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+// Function to close sidebar
+function closeSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
