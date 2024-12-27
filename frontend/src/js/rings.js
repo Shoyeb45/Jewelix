b538a89efe6c716483fe4a1d2914249995bedebc
Shoyeb45 <shoyebff45@gmail.com>
2024-12-27 23:00:41 +0530

    rings

--- frontend/src/js/rings.js
+++ frontend/src/js/rings.js
@@ -1,9 +1,11 @@
 async function getProductData() {
     try {
         const response = await fetch("http://localhost:4000/api/v1/product/getProduct");
+        console.log(response);
         const productData = await response.json();
-        
         console.log(productData);
+        console.log(productData[0].productName);
+        
         
     } catch (err) {
         console.log(err?.message);
